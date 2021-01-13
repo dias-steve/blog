2c2876fcc86e34b0d1030dbb2aa771f8a920914d
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-13 15:35:55 +0100

    Creating of IndexScreen

---
+++ screens/IndexScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const IndexScreen = () => {
+    return <Text>IndexScreen</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+export default IndexScreen;
