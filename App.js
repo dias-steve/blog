01e3ec3cc120eb1c003b80ee961c09d88975e666
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-13 16:20:32 +0100

    Moving all files into src directory

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import IndexScreen from './screens/IndexScreen';
+import IndexScreen from './src/screens/IndexScreen';
+import {BlogProvider} from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
