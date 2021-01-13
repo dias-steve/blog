fc68167d97ebdd679426b1e19a7542cf880a0f51
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-13 15:58:36 +0100

    Context supported by App

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './screens/IndexScreen';
@@ -14,4 +14,9 @@ const navigator = createStackNavigator(
     }
   }
 );
-export default createAppContainer(navigator);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App/>;
+}
